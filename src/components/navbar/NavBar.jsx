b70efc3f49d59dedb49c4a834b5b97f12055920e
Rakesh Kundu <159798591+Raxku2@users.noreply.github.com>
2026-01-26 00:32:12 +0530

    Add About page and enhance UI components
    Introduced a new About page component and route. Improved NavBar with user profile display and tooltips. Refactored Settings with tabbed navigation and theme previews. Updated FileOpen for better layout and styling. Added 'clsx' dependency for conditional class management.

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,20 +1,51 @@
 import { h, Component } from 'preact';
 import { RiGithubLine } from '@remixicon/react';
 import { route } from 'preact-router';
+import tippy from 'tippy.js';
+import { UserStore } from '../../store';
+import { useEffect, useRef, useState } from 'preact/hooks';
+
 
 
 const NavBar = () => {
-    const PROJECT_REPO = import.meta.env.VITE_PROJECT_REPO
+    const PROJECT_REPO = import.meta.env.VITE_PROJECT_REPO;
+    const { userLogd, userName } = UserStore();
+    const [userDp, setUserDp] = useState(false);
+    const [userDpText, setUserDpText] = useState('G');
+    const dpref = useRef(null);
+
+    useEffect(() => {
+        if (userLogd) {
+            setUserDp(true);
+            setUserDpText(userName[0]);
+            tippy(dpref.current, {
+                content: `Hello! ${userName}`
+            })
+        } else {
+            setUserDp(false);
+            setUserDpText("");
+        }
+    }, [userLogd])
+
+    useEffect(() => {
+
+        tippy(dpref.current, {
+            content: `Hello! ${userName}`
+        })
+    }, [])
+
     return (
         <div class=" p-1 text-2xl justify-between flex items-center  ">
 
 
             <div class="flex cursor-pointer"
                 onClick={e => { route('/') }}>
-                <h1>GOAT</h1>
+                <h1 class="font-black">GOAT <span class="text-(--border-color)">MARKDOWN</span></h1>
+                
             </div>
 
-            <div class="flex justify-between text-sm items-center">
+            <div class="flex justify-between text-sm items-center h-full">
+
                 <button
                     type="button"
                     class="cursor-pointer p-2 mx-2 text-(--border-color) "
@@ -28,12 +59,29 @@ const NavBar = () => {
 
                 <button
                     type="button"
+                    hidden={userDp}
                     class="cursor-pointer p-2 bg-(--button-bg) rounded-sm border-solid border-2 border-(--border-color) "
                     onClick={e => {
                         route('/settings')
                     }}
                 >
-                    Profile
+                    Settings
+                </button>
+
+
+
+
+                <button
+                    type="button"
+                    class="cursor-pointer  ml-1 h-10 w-10 flex justify-center items-center rounded-full bg-(--border-color)/20 text-(--border-color) border-2 border-(--border-color)/70 "
+                    hidden={!userDp}
+                    ref={dpref}
+                    onClick={e => {
+                        route('/settings')
+                    }}
+                >
+                    <h1 class="text-2xl">{userDpText}</h1>
+
                 </button>
 
 
--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,7 +1,11 @@
 import { h, Component } from 'preact';
-import { RiAccountCircle2Line, RiLoginBoxFill, RiUserAddFill } from "@remixicon/react";
+import { RiAccountCircle2Line, RiInformationFill, RiPaletteFill, RiUserFill } from "@remixicon/react";
 import { useTheme } from '../../hooks';
 import { UserStore } from '../../store';
+import { useState } from 'preact/hooks';
+import { useEffect } from 'react';
+import clsx from 'clsx'
+import { route } from 'preact-router';
 
 const Settings = () => {
     const { userName, userLogd, userSignout } = UserStore();
@@ -15,107 +19,270 @@ const Settings = () => {
 
     const { themes, theme, setTheme } = useTheme();
 
+    const [profileSettingsVisibility, setProfileSettingsVisibility] = useState(true);
+    const [appearanceSettingsVisibility, setAppearanceSettingsVisibility] = useState(false);
+
+    const [userNameCard, setUserNameCard] = useState("User");
+    useEffect(() => {
+        if (userLogd) {
+            setUserNameCard(userName);
+        } else {
+            setUserNameCard("User");
+        }
+    }, [userLogd]);
+
+
 
     return (
-        <div class="flex justify-center  ">
-            <div class="w-full md:w-[70dvw] lg:w-[60dvw] p-4 ">
-                <div class="overflow-y-scroll">
+        <div class="flex w-full h-full flex-col md:flex-row">
 
-                    <div class=" ">
-                        <div class="flex justify-center content-center " >
-                            <RiAccountCircle2Line size={'30%'} />
+            <div class=" md:w-[25%] lg:w-[16%] md:border-r border:border-(--border-color) flex md:flex-col flex-wrap items-center justify-center md:justify-normal md:p-0 ">
+
+                <button
+                    class={clsx("p-2 rounded-lg md:rounded-xl border-2 border-(--border-color) md:border-(--bg-color) hover:border-(--border-color) hover:bg-(--button-bg) flex items-center text-sm lg:text-lg w-[46%] cursor-pointer m-1 md:w-[80%]",
+                        profileSettingsVisibility ? "bg-(--button-bg) " : "bg-none")}
+
+                    onClick={() => {
+                        setProfileSettingsVisibility(true);
+                        setAppearanceSettingsVisibility(false);
+                    }}
+                >
+                    <RiUserFill size={20} />
+                    <h1 class="mx-4">Profile</h1>
+                </button>
+
+                <button
+                    class={clsx("p-2 rounded-lg md:rounded-xl border-2 border-(--border-color) md:border-(--bg-color) hover:border-(--border-color) hover:bg-(--button-bg) flex items-center text-sm lg:text-lg w-[46%] cursor-pointer m-1 md:w-[80%]",
+                        appearanceSettingsVisibility ? "bg-(--button-bg) " : "bg-none")}
+                    onClick={() => {
+                        setProfileSettingsVisibility(false);
+                        setAppearanceSettingsVisibility(true);
+                    }}
+                >
+                    <RiPaletteFill size={20} />
+                    <h1 class="mx-4">Appearance</h1>
+                </button>
+
+                <button
+                    class={clsx("p-2 rounded-lg md:rounded-xl border-2 border-(--border-color) md:border-(--bg-color) hover:border-(--border-color) hover:bg-(--button-bg) flex items-center text-sm lg:text-lg w-[46%] cursor-pointer m-1 md:w-[80%]")}
+                    onClick={() => {
+                        route('/about');
+                    }}
+                >
+                    <RiInformationFill size={20} />
+                    <h1 class="mx-4">About</h1>
+                </button>
+
+            </div>
+            <div class=" md:w-[75%] lg:w-[84%]">
+                <div class="w-full h-full" hidden={!profileSettingsVisibility}>
+                    <div class="px-4 md:px-8 my-8 ">
+                        <h1 class="text-lg md:text-2xl font-black">Profile Settings</h1>
+                    </div>
+
+                    <div class="mx-8 my-4 overflow-hidden flex flex-col">
+                        <div class="w-full bg-(--button-bg) p-6 rounded-2xl ">
+                            <div class="flex flex-col md:flex-row justify-center md:justify-normal  items-center border-b-2 border-(--border-color)/40 ">
+                                <div>
+                                    <RiAccountCircle2Line size={140} />
+                                </div>
+                                <div class="p-4 content-center">
+                                    <h1 class="font-bold text-2xl">{userNameCard}</h1>
+                                </div>
+                            </div>
+                            <div class="mt-6 flex justify-between"
+                                hidden={true}>
+                                <div class="w-[49%]">
+                                    <h1 class="text-sm font-bold">
+                                        FULL NAME
+                                    </h1>
+                                    <input type="text" name="" id="" class="bg-(--border-color)/10 my-2 w-full rounded-lg text-lg py-2 px-4" disabled value={userName} />
+                                </div>
+                                <div class="w-[49%]">
+                                    <h1 class="text-sm font-bold">
+                                        EMAIL ADDRESS
+                                    </h1>
+                                    <input type="text" name="" id="" class="bg-(--border-color)/10 my-2 w-full rounded-lg text-lg py-2 px-4" disabled value={userName} />
+                                </div>
+                            </div>
                         </div>
-                        <div class="flex justify-center">
-                            <h1 class="text-4xl font-bold">{userName}</h1>
+
+                    </div>
+                    <div>
+
+                        <div class="px-4 md:px-8 my-8 ">
+                            <h1 class="text-lg md:text-xl  font-bold">Account Actions</h1>
                         </div>
-                    </div>
 
-                    <div class="flex justify-center items-center p-4">
-                        <h1 class="px-2 text-xl">Theme:</h1>
-                        <select name="theme" id="theme" class="rounded-sm  border-2 p-2 border-(--bg-color) hover:border-(--border-color)"
-                            onChange={e => {
-                                console.log(e.target.value);
-                                setTheme(e.target.value.toLowerCase())
-
-                            }}
-                        >
-                            <option value={theme} selected={true}>{theme.toUpperCase()}</option>
-                            {themes.map(e => {
-                                if (e.id.toUpperCase() != theme.toUpperCase()) {
-
-                                    return (<option value={e.name}>{e.name.toUpperCase()} </option>)
-                                }
-
-                            })}
-                        </select>
-                    </div>
-
-                    <div class="flex justify-center items-center">
-                        <hr class="w-[50%] h-1 rounded bg-(--border-color) outline-0 border-0   " />
-                    </div>
-
-                    <div class="flex justify-center p-2 ">
-
-                        <a href="http://" class="rounded-sm  border-2 p-2 border-(--bg-color) hover:border-(--border-color) text-white/20" > About </a>
-                    </div>
-
-                    <div class="flex justify-center items-center">
-                        <hr class="w-[50%] h-1 rounded bg-(--border-color) outline-0 border-0   " />
-                    </div>
-
-                    <div class="flex justify-center p-2 ">
-
-                        <a href="http://" class="rounded-sm  border-2 p-2 border-(--bg-color) hover:border-(--border-color) text-white/20" > Keyboard Shortcuts </a>
-                    </div>
-
-                    <div class="flex justify-center items-center">
-                        <hr class="w-[50%] h-1 rounded bg-(--border-color) outline-0 border-0   " />
-                    </div>
-
-
-                    <div class="flex justify-center p-2 " hidden={userLogd}>
-
-                        <div class="w-[90%] md:w-[60%] lg:w-[20%]">
-                            <button class="p-4 rounded border-2  border-(--border-color) md:border-(--bg-color) hover:border-(--border-color) hover:bg-(--button-bg)  flex justify-center items-center text-2xl cursor-pointer m-1 w-full"
+                        <div class="mx-8 my-4 overflow-hidden flex justify-center items-center md:justify-normal md:items-start">
+                            <button
+                                class="bg-(--button-bg) h-14 w-36 rounded-xl font-bold mr-4 cursor-pointer border-2 border-transparent hover:border-(--border-color)"
                                 onClick={() => {
                                     window.location.replace(signin_url);
-                                }}>
-                                <h1 class="mx-4">Signin</h1>
-                                <RiLoginBoxFill />
+                                }}
+                                hidden={userLogd}
+                            >
+
+                                Sign In
                             </button>
-
-                        </div>
-
-                        <div class="w-[90%] md:w-[60%] lg:w-[20%]">
-                            <button class="p-4 rounded border-2  border-(--border-color) md:border-(--bg-color) hover:border-(--border-color) hover:bg-(--button-bg)  flex justify-center items-center text-2xl cursor-pointer m-1 w-full "
+                            <button
+                                class="bg-(--button-bg) h-14 w-36 rounded-xl font-bold mr-4 cursor-pointer border-2 border-transparent hover:border-(--border-color)"
                                 onClick={e => {
                                     window.location.replace(signup_url);
                                 }}
+                                hidden={userLogd}
                             >
-                                <h1 class="mx-4">Signup</h1>
-                                <RiUserAddFill />
+                                Sign Up
                             </button>
-
-                        </div>
-                    </div>
-
-                    <div class="flex justify-center p-2 " hidden={!userLogd}>
-                        <div class="w-[90%] md:w-[60%] lg:w-[20%]">
-                            <button class="p-4 rounded border-2  border-(--border-color) md:border-(--bg-color) hover:border-(--border-color) hover:bg-(--button-bg)  flex justify-center items-center text-2xl cursor-pointer m-1 w-full "
+                            <button
+                                class="bg-(--button-bg) h-14 w-36 rounded-xl font-bold mr-4 cursor-pointer border-2 border-transparent hover:border-(--border-color)"
                                 onClick={e => {
                                     userSignout();
                                     localStorage.removeItem('userData')
                                 }}
+                                hidden={!userLogd}
                             >
-                                <h1 class="mx-4">Signoff</h1>
-                                <RiUserAddFill />
+                                Sign Out
                             </button>
-
                         </div>
                     </div>
                 </div>
 
+
+                <div class="h-full w-full" hidden={!appearanceSettingsVisibility}>
+                    <div class="px-4 md:px-8 my-8 ">
+                        <h1 class="text-lg md:text-2xl font-black">Appearance Settings</h1>
+                    </div>
+
+                    <div class="mx-8 my-4 flex flex-wrap">
+
+                        <div class={clsx("bg-[#F9F8F6] h-24 w-38 md:h-40 md:w-60 mr-4 mb-4 rounded-xl border-4 overflow-hidden cursor-pointer ", theme == 'light' ? "border-(--border-color)" : "border-transparent")}
+                            onClick={() => {
+                                setTheme('light')
+                            }}
+                        >
+                            <div class="w-full h-full flex flex-col p-2">
+                                <div class="w-full h-[20%] py-1 rounded-2xl">
+                                    <div class="rounded bg-[#EFE9E3] h-full">
+
+                                    </div>
+                                </div>
+                                <div class="w-full h-[80%] flex">
+                                    <div class="w-[25%] pt-1 pr-1">
+                                        <div class="rounded bg-[#EFE9E3] h-full w-full"></div>
+                                    </div>
+                                    <div class="w-[75%] pt-1 pl-1">
+                                        <div class="rounded  h-full w-full p-1 border border-[#D9CFC7]">
+                                            <div class="rounded-full bg-[#b6946a] h-3 mb-1 w-[60%]"></div>
+                                            <div class="rounded-full bg-[#b6946a] h-3 w-[40%]"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class={clsx("bg-[#222831] h-24 w-38 md:h-40 md:w-60 mr-4 mb-4 rounded-xl border-4 overflow-hidden cursor-pointer ", theme == 'dark' ? "border-(--border-color)" : "border-transparent")}
+                            onClick={() => {
+                                setTheme('dark')
+                            }}>
+                            <div class="w-full h-full flex flex-col p-2">
+                                <div class="w-full h-[20%] py-1 rounded-2xl">
+                                    <div class="rounded bg-[#31363F] h-full">
+
+                                    </div>
+                                </div>
+                                <div class="w-full h-[80%] flex">
+                                    <div class="w-[25%] pt-1 pr-1">
+                                        <div class="rounded bg-[#31363F] h-full w-full"></div>
+                                    </div>
+                                    <div class="w-[75%] pt-1 pl-1">
+                                        <div class="rounded  h-full w-full p-1 border border-[#76ABAE]">
+                                            <div class="rounded-full bg-[#EEEEEE] h-3 mb-1 w-[60%]"></div>
+                                            <div class="rounded-full bg-[#EEEEEE] h-3 w-[40%]"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class={clsx("bg-[#FFC4C4] h-24 w-38 md:h-40 md:w-60 mr-4 mb-4 rounded-xl border-4  overflow-hidden cursor-pointer ", theme == 'pink' ? "border-(--border-color)" : "border-transparent")}
+                            onClick={() => {
+                                setTheme('pink')
+                            }}>
+                            <div class="w-full h-full flex flex-col p-2">
+                                <div class="w-full h-[20%] py-1 rounded-2xl">
+                                    <div class="rounded bg-[#FCF5EE] h-full">
+
+                                    </div>
+                                </div>
+                                <div class="w-full h-[80%] flex">
+                                    <div class="w-[25%] pt-1 pr-1">
+                                        <div class="rounded bg-[#FCF5EE] h-full w-full"></div>
+                                    </div>
+                                    <div class="w-[75%] pt-1 pl-1">
+                                        <div class="rounded  h-full w-full p-1 border border-[#EE6983]">
+                                            <div class="rounded-full bg-[#850E35] h-3 mb-1 w-[60%]"></div>
+                                            <div class="rounded-full bg-[#850E35] h-3 w-[40%]"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class={clsx("bg-[#a4c496] h-24 w-38 md:h-40 md:w-60 mr-4 mb-4 rounded-xl border-4 overflow-hidden cursor-pointer ", theme == 'green' ? "border-(--border-color)" : "border-transparent")}
+                            onClick={() => {
+                                setTheme('green')
+                            }}>
+                            <div class="w-full h-full flex flex-col p-2">
+                                <div class="w-full h-[20%] py-1 rounded-2xl">
+                                    <div class="rounded bg-[#8ca481] h-full">
+
+                                    </div>
+                                </div>
+                                <div class="w-full h-[80%] flex">
+                                    <div class="w-[25%] pt-1 pr-1">
+                                        <div class="rounded bg-[#8ca481] h-full w-full"></div>
+                                    </div>
+                                    <div class="w-[75%] pt-1 pl-1">
+                                        <div class="rounded  h-full w-full p-1 border border-[#d9ead3]">
+                                            <div class="rounded-full bg-[#8b5d12] h-3 mb-1 w-[60%]"></div>
+                                            <div class="rounded-full bg-[#8b5d12] h-3 w-[40%]"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class={clsx("bg-[#1B3C53] h-24 w-38 md:h-40 md:w-60 mr-4 mb-4 rounded-xl border-4 overflow-hidden cursor-pointer", theme == 'blue' ? "border-(--border-color)" : "border-transparent")}
+                            onClick={() => {
+                                setTheme('blue')
+                            }}>
+                            <div class="w-full h-full flex flex-col p-2">
+                                <div class="w-full h-[20%] py-1 rounded-2xl">
+                                    <div class="rounded bg-[#234C6A] h-full">
+
+                                    </div>
+                                </div>
+                                <div class="w-full h-[80%] flex">
+                                    <div class="w-[25%] pt-1 pr-1">
+                                        <div class="rounded bg-[#234C6A] h-full w-full"></div>
+                                    </div>
+                                    <div class="w-[75%] pt-1 pl-1">
+                                        <div class="rounded  h-full w-full p-1 border border-[#456882]">
+                                            <div class="rounded-full bg-[#D2C1B6] h-3 mb-1 w-[60%]"></div>
+                                            <div class="rounded-full bg-[#D2C1B6] h-3 w-[40%]"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
+
+
         </div>
     );
 };
